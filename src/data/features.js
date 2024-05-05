882b3c397fc269fa3366bc478f031b4ecb8444b0
Nikos Kechris <nkechris22@gmail.com>
2024-05-06 00:03:43 +0300

    Move svgs to public folder

--- src/data/features.js
+++ src/data/features.js
@@ -3,17 +3,17 @@ export default {
     {
       title: "Track crypto prices",
       description: "Explore a vast collection of cryptocurrencies from around the world. Stay updated with real-time prices, market capitalization, trading volume, and more for each cryptocurrency.",
-      image: "/src/assets/all-coins.svg",
+      image: "all-coins.svg",
     },
     {
       title: "Search for a specific crypto",
       description: "Effortlessly search for any cryptocurrency by name. Find detailed information including historical data, technical analysis, and community sentiment for each searched cryptocurrency.",
-      image: "/src/assets/search-coin.svg",
+      image: "search-coin.svg",
     },
     {
       title: "Add coins to your favorite list",
       description: "Personalize your experience by adding your favorite cryptocurrencies to your watchlist. Easily track their performance, receive price alerts, and stay informed about the latest news and developments.",
-      image: "/src/assets/favorite-coin.svg",
+      image: "favorite-coin.svg",
     },
   ],
 };
